4abb9ed5839891c2fd5f2dab0ba9da7daa2a7231
Hadis Samadi <samadi.hadis@gmail.com>
2023-12-09 12:18:30 +0330

    Add login page

---
+++ app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/login/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.samadihadis.imdbvideoapplication.presentation.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.samadihadis.imdbvideoapplication.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
+    private fun setOnClickListener() {
+        binding.loginButton.setOnClickListener {
+            val userName = binding.userNameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            if (userName.isEmpty() || password.isEmpty()) {
+                binding.userNameEditText.error = "Please Enter User Name Or Password"
+            }
+        }
+    }
+}
