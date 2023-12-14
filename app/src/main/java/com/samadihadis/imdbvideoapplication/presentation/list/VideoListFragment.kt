72bc4816ffc5b796ebe2376dc8c2d4bc4b07b40f
Hadis Samadi <samadi.hadis@gmail.com>
2023-12-14 22:19:09 +0330

    Add Recycle View and Divider Item Decoration

--- app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/list/VideoListFragment.kt
+++ app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/list/VideoListFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.samadihadis.imdbvideoapplication.databinding.FragmentVideoListBinding
 
 class VideoListFragment : Fragment() {
 
     private lateinit var binding: FragmentVideoListBinding
+    val videoList = mutableListOf<VideoModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +25,21 @@ class VideoListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initialRecycleView()
+    }
 
+
+    private fun initialRecycleView() {
+        val recycleView = binding.recyclerViewVideo
+
+        recycleView.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        val dividerItemDecoration =
+            DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        recycleView.addItemDecoration(dividerItemDecoration)
+
+        val musicAdaptor = VideoAdaptor(videoList, requireContext())
+        recycleView.adapter = musicAdaptor
     }
 
 }
