eaf696a596374f1bf8c26eeeb7da84fc2fd63c84
Hadis Samadi <h.kheirollahpour@bluteam.ir>
2023-12-24 18:04:46 +0330

    Redesign video detail page

--- app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/intro/IntroFragment.kt
+++ app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/intro/IntroFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.samadihadis.imdbvideoapplication.R
 import com.samadihadis.imdbvideoapplication.databinding.FragmentIntroBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -19,7 +20,7 @@ class IntroFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentIntroBinding.inflate(inflater, container, false)
         return binding.root
     }
