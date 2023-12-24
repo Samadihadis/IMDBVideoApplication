fa53049931bf1b4a950d6a03589eaffe98764c93
h.kheirollahpour <h.kheirollahpour@bluteam.ir>
2023-12-24 16:59:20 +0330

    Refactor video adapter

--- app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/list/VideoAdaptor.kt
+++ app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/list/VideoAdaptor.kt
@@ -1,64 +1,39 @@
 package com.samadihadis.imdbvideoapplication.presentation.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.samadihadis.imdbvideoapplication.R
 import com.samadihadis.imdbvideoapplication.data.VideoModel
-import androidx.constraintlayout.widget.ConstraintLayout as ConstraintLayout
 
 class VideoAdaptor(
     private var videoList: List<VideoModel>,
     private val navController: NavController,
+    ) : RecyclerView.Adapter<VideoItemViewHolder>() {
 
-    ) :
-    RecyclerView.Adapter<VideoAdaptor.ViewHolder>() {
-
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        val image: ImageView
-        val title: TextView
-        val description: TextView
-        val isFavorite: ImageView
-        val goDetail: ImageView
-        val rootLayout: ConstraintLayout
-
-
-        init {
-            view.apply {
-                image = findViewById(R.id.videoImageView)
-                title = findViewById(R.id.titleTextView)
-                description = findViewById(R.id.descriptionTextView)
-                isFavorite = findViewById(R.id.isFavorite)
-                goDetail = findViewById(R.id.goDetail)
-                rootLayout = findViewById(R.id.itemVideo)
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoItemViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_video, parent, false)
-        return ViewHolder(view)
+        return VideoItemViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return videoList.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: VideoItemViewHolder, position: Int) {
         holder.apply {
-            title.text = videoList[position].title
-            description.text = videoList[position].description
-            //Glide.with(context)
-              //  .load(videoList[position].bannerImageLink)
-                //.error(R.drawable.video)
-                //.transform(CenterCrop(), RoundedCorners(40))
-                //.into(image)
+            titleTextView.text = videoList[position].title
+            descriptionTextView.text = videoList[position].description
+            Glide.with(holder.rootLayout.context)
+                .load(videoList[position].bannerImageLink)
+                .error(R.drawable.icon_video)
+                .transform(CenterCrop(), RoundedCorners(40))
+                .into(avatarImageView)
             rootLayout.setOnClickListener {
                 navController.navigate(
                     VideoListFragmentDirections.actionToVideoDetailFragment(
---
+++ app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/list/VideoItemViewHolder.kt
@@ -0,0 +1,27 @@
+package com.samadihadis.imdbvideoapplication.presentation.list
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.samadihadis.imdbvideoapplication.R
+
+class VideoItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val avatarImageView: ImageView
+    val titleTextView: TextView
+    val descriptionTextView: TextView
+    val favoriteImageView: ImageView
+    val rootLayout: ConstraintLayout
+
+    init {
+        view.apply {
+            avatarImageView = findViewById(R.id.videoImageView)
+            titleTextView = findViewById(R.id.titleTextView)
+            descriptionTextView = findViewById(R.id.descriptionTextView)
+            favoriteImageView = findViewById(R.id.favoriteImageView)
+            rootLayout = findViewById(R.id.itemVideo)
+        }
+    }
+}
