c9b7b9bf2d2d26f5c520c14af642ffe0ebc9b8e8
Hadis Samadi <samadi.hadis@gmail.com>
2023-12-26 19:03:32 +0330

    Add request api call in video list page

--- app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/list/VideoListFragment.kt
+++ app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/list/VideoListFragment.kt
@@ -10,20 +10,21 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.samadihadis.imdbvideoapplication.data.VideoModel
+import com.google.gson.Gson
+import com.samadihadis.imdbvideoapplication.data.MovieModel
+import com.samadihadis.imdbvideoapplication.data.PopularMovieModel
 import com.samadihadis.imdbvideoapplication.databinding.FragmentVideoListBinding
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.json.JSONObject
 import java.io.IOException
 
 class VideoListFragment : Fragment() {
 
     private lateinit var binding: FragmentVideoListBinding
-    private var videoList = mutableListOf<VideoModel>()
+    private var movieList = listOf<MovieModel>()
     private var videoAdaptor : VideoAdaptor? = null
 
     override fun onCreateView(
@@ -36,25 +37,58 @@ class VideoListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         cleanList()
         initialRecycleView()
+        getData()
         // https://github.com/Samadihadis/WeatherApplication/blob/master/app/src/main/java/com/samadihadis/weatherapplication/MainActivity.kt
-
     }
 
     private fun initialRecycleView() {
-        val recycleView = binding.recyclerViewVideo
-
-        recycleView.layoutManager =
-            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
-        val dividerItemDecoration =
-            DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
-        recycleView.addItemDecoration(dividerItemDecoration)
-
-        videoAdaptor = VideoAdaptor(videoList, findNavController())
-        recycleView.adapter = videoAdaptor
+        binding.recyclerViewVideo.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            val dividerItemDecoration =
+                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
     }
+
+    private fun setupAdapter() {
+        videoAdaptor = VideoAdaptor(movieList, findNavController())
+        binding.recyclerViewVideo.adapter = videoAdaptor
+    }
+
     private fun cleanList() {
-        videoList.clear()
+        movieList = listOf()
         videoAdaptor?.notifyDataSetChanged()
+    }
+
+
+    private fun getData() {
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://api.themoviedb.org/3/movie/popular?api_key=8f2b52cb3578ed865acfbd4d642dc062")
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                Log.d("tagx", "onFailure:failed")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val rawContent = response.body!!.string()
+                val result = getDataAndShowThem(rawContent)
+                movieList = result.results
+                requireActivity().runOnUiThread {
+                    setupAdapter()
+                }
+            }
+        })
+    }
+
+    private fun getDataAndShowThem(rawData: String): PopularMovieModel {
+        val gson = Gson()
+        val obj : PopularMovieModel = gson.fromJson(rawData, PopularMovieModel::class.java)
+        return obj
     }
 
 }
