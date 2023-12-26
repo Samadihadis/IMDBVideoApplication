3313acdbe35d9346fac90f2148e901f2749994e4
Hadis Samadi <samadi.hadis@gmail.com>
2023-12-26 18:39:26 +0330

    Add some dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,14 +41,16 @@ android {
 
 dependencies {
 
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("com.google.android.material:material:1.10.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
