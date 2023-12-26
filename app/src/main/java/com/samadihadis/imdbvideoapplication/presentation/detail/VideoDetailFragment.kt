74cc684b9a2b79f2fe8125db33148b8133ab8152
Hadis Samadi <samadi.hadis@gmail.com>
2023-12-26 20:01:33 +0330

    Refactor completely video detail page

--- app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/detail/VideoDetailFragment.kt
+++ app/src/main/java/com/samadihadis/imdbvideoapplication/presentation/detail/VideoDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.samadihadis.imdbvideoapplication.R
 import com.samadihadis.imdbvideoapplication.databinding.FragmentVideoDetailBinding
 
@@ -26,15 +27,14 @@ class VideoDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            toolbar.title = args.videoModel.title
-            filmDescriptionId.text = args.videoModel.description
-            filmAuthorId.text = args.videoModel.author
-            filmBuildYearId.text = args.videoModel.buildYear.toString()
-            if (args.videoModel.isFavorite) {
-                favoriteImageView.setImageResource(R.drawable.icon_fav_fill)
-            } else {
-                favoriteImageView.setImageResource(R.drawable.icon_fav_empty)
-            }
+            toolbar.title = args.movieModel.title
+            descriptionValueTextView.text = args.movieModel.overview
+            yearValueTextView.text = args.movieModel.release_date
+            adultValueTextView.text = args.movieModel.adult.toString()
+            Glide.with(requireContext())
+                .load("https://image.tmdb.org/t/p/w5005" +args.movieModel.backdrop_path)
+                .error(R.drawable.banner_image_placeholder)
+                .into(bannerImageView)
         }
     }
 
